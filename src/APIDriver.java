60956f5f55ae693e9ba965702baeb089801f6025
Ken Sodetz <kensodetz@gmail.com>
2017-09-29 23:56:15 -0400

    Initial commit

---
+++ src/APIDriver.java
@@ -0,0 +1,16 @@
+/**
+ * The APIDriver class gets user input and calls the API to get the statistics
+ *
+ * @author Ken Sodetz
+ * @since 9/29/2017
+ */
+public class APIDriver {
+
+    /**
+     * Main Method
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
