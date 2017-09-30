39531a1b234c70cb8f3e0379a95be975f12600d9
Ken Sodetz <kensodetz@gmail.com>
2017-09-30 02:58:29 -0400

    parsing clean up

--- src/APIDriver.java
+++ src/APIDriver.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
 
 /**
