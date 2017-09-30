b0d1ac0c1ddf6493ee4a983ecf4af925cc3cd38b
Shiv <paul81@purdue.edu>
2017-09-30 16:15:07 -0400

    add calculate.java

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,4 +1,38 @@
 package Gastimator.src;
 
-public class Calculate {
+public class Calculate
+{
+    static double getCityMPG()      //from APIDriver
+    {
+        return 30.0;
+    }
+
+    static double getHwMPG()        //from APIDriver
+    {
+        return 50.0;
+    }
+
+    static double getDistance()     //from DistanceFinder
+    {
+        return 1000.0;
+    }
+
+    public static void main(String[] args)
+    {
+        double CityMPG = getCityMPG();
+        double HwMPG = getHwMPG();
+        double distance = getDistance();
+        double gasReq;
+
+        if(distance>40)
+        {
+            gasReq = distance/HwMPG;
+        }
+        else
+        {
+            gasReq = distance/CityMPG;
+        }
+
+        System.out.println("Gas required is " + gasReq+ " gallons.");
+    }
 }
