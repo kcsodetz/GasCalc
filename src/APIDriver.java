87460fa6a17953c0163b1870513073ee3f95105d
Ken Sodetz <kensodetz@gmail.com>
2017-09-30 02:27:43 -0400

    url response can be parsed

--- src/APIDriver.java
+++ src/APIDriver.java
@@ -3,6 +3,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.DoubleSummaryStatistics;
 import java.util.Scanner;
 
 /**
@@ -13,20 +14,28 @@ import java.util.Scanner;
  */
 public class APIDriver {
 
+    private static final String APIKEY = "A8874x8oBWR0GdYXGccI2tYFFULXur7a";
 
-    public APIDriver(){
+    /**
+     * Default constructor
+     */
+    private APIDriver(){
 
     }
 
-    //https://apis.solarialabs.com/shine/v1/vehicle-stats/specs?make={value}&model={value}&year={value}&full-data={value}&apikey={value}
-
     /**
-     * Connects to API using an http request
+     * Connects the API to git
+     * @param url url for the http call
+     * @param make make of the vehicle
+     * @param model model of the vehicle
+     * @param year year of the vehicle
+     * @param key API key
+     * @return responseBody, string response of the API call
      */
-    public void connectToAPI(String url, String make, String model, String year, String key){
+    public String connectToAPI(String url, String make, String model, String year, String key) {
         String charset = "UTF-8";
         InputStream response = null;
-        URLConnection connection = null;
+        URLConnection connection;
         try {
             connection = new URL(url+"make="+make+"&model="+model+"&year="+year+"&apikey="+key).openConnection();
             connection.setRequestProperty("Accept-Charset", charset);
@@ -34,23 +43,42 @@ public class APIDriver {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        assert response != null;
         try(Scanner scanner = new Scanner(response)){
             String responseBody = scanner.useDelimiter("\\A").next();
             System.out.println(responseBody);
+            return responseBody;
         }
+    }
+
+    public double getCityMPG(String apiCallString) {
+
+        String response = apiCallString.substring(apiCallString.indexOf("City_Unadj_Conventional_Fuel"), apiCallString.indexOf("Hwy_Unadj_Conventional_Fuel"));
+        response = response.substring(response.indexOf(":") + 1, response.indexOf(","));
+        return Double.parseDouble(response);
+    }
+
+    public double getHighwayMPG(String apiCallString) {
+        String response = apiCallString.substring(apiCallString.indexOf("Hwy_Unadj_Conventional_Fuel"), apiCallString.indexOf("Air_AspirMethod"));
+        response = response.substring(response.indexOf(":") + 1, response.indexOf(","));
+        return Double.parseDouble(response);
     }
 
     /**
      * Main Method
-     * @param args
+     * @param args default arguments
      */
     public static void main(String[] args) {
         APIDriver apiDriver = new APIDriver();
         String url = "https://apis.solarialabs.com/shine/v1/vehicle-stats/specs?";
-        String make = "Honda";
-        String model = "CR-V";
+        String make = "honda";
+        String model = "cr-v";
         String year = "2012";
-        String APIKey = "A8874x8oBWR0GdYXGccI2tYFFULXur7a";
-        apiDriver.connectToAPI(url, make, model, year, APIKey);
+        double cityMPG;
+        double highwayMPG;
+        String apiCallString = apiDriver.connectToAPI(url, make, model, year, APIKEY);
+        cityMPG = apiDriver.getCityMPG(apiCallString);
+        highwayMPG = apiDriver.getHighwayMPG(apiCallString);
+        System.out.println("City MPG: "+cityMPG+"\n"+"Highway MPG: "+highwayMPG);
     }
 }
